47f4374dd748032f11c4db20f2e7f2ef67d0295c
Marcelearn <jaworskimarcel@gmail.com>
2022-11-22 15:39:30 +0100

    React-Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
      <h1>My Quiz</h1>
     </div>
   );
