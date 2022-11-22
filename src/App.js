dfcb3f67b81fa4adb120744262c221977923f94e
monsif <monsif@gmail.com>
2022-11-22 14:17:31 +0100

    Datajson imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Fotter from "./componens/Footer"
+import Header from "./componens/Header"
+import DataJson from "./data.json"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Fotter/>
+      <Header/>
+      <DataJson/>
+      
     </div>
   );
 }
