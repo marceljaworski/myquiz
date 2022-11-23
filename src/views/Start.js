076510c1a945091f47f7b4b71627f9b26499780e
Marcelearn <jaworskimarcel@gmail.com>
2022-11-23 10:13:11 +0100

    question component verbunden

--- src/views/Start.js
+++ src/views/Start.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Start() {
   return (
-    <div>
+    <div className='Start'>
         
         <input type= "text" placeholder= "schreib dein Name"></input>
     </div>
