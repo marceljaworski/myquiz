21867dbfc158ed44d987d95fb3576affe136f9bc
Marcelearn <jaworskimarcel@gmail.com>
2022-11-23 11:53:45 +0100

    count an questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,14 +5,16 @@ import Data from '../data.json'
 
 export default function Questions() {
   const [fragen] = useState(Data.Fragen)
+  const [count, setCount] = useState(0)
+  console.log(count)
   const handleClick = () => {
-    
+    setCount(count + 1)
   }
   return (
     <div>
-      <h1>{fragen[0].Frage}</h1>
+      <h1>{fragen[count].Frage}</h1>
       
-      {fragen[0].Antworten.map((el, index) => {
+      {fragen[count].Antworten.map((el, index) => {
         
         return <button onClick={handleClick} key={index}>{el}</button>
       })}
