75e92af9b34e71130b29572d43b36f650cfc00b1
Marcelearn <jaworskimarcel@gmail.com>
2022-11-23 11:57:18 +0100

    dolar counter

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,13 +6,17 @@ import Data from '../data.json'
 export default function Questions() {
   const [fragen] = useState(Data.Fragen)
   const [count, setCount] = useState(0)
+  const [ money, setMoney ] = useState(100)
   console.log(count)
   const handleClick = () => {
     setCount(count + 1)
+    setMoney(money + 100)
   }
   return (
     <div>
+      <h2>{money}$</h2>
       <h1>{fragen[count].Frage}</h1>
+
       
       {fragen[count].Antworten.map((el, index) => {
         
