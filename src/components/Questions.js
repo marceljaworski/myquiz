37ca2eaf25eae0d2d0713277db9e55d1dc0cbc83
Marcelearn <jaworskimarcel@gmail.com>
2022-11-23 10:40:12 +0100

    map data component Questionbuttons mit antworten

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Data from '../data.json'
+
 
 export default function Questions() {
+  const [fragen] = useState(Data.Fragen)
+  const handleClick = () => {
+    
+  }
   return (
     <div>
-      <h1>Questions</h1>
-      <p>Was ist Hauptstadt von USA?</p>
+      <h1>{fragen[0].Frage}</h1>
+      {fragen[0].Antworten.map((el, index) => {
+        
+        return <button onClick={handleClick} key={index}>{el}</button>
+      })}
+      
     </div>
   )
 }
