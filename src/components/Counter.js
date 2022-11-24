e7fd654d8f4c45564846df040a430391dbc8e8b4
monsif <monsif@gmail.com>
2022-11-24 14:56:40 +0100

    time

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
 
-export default function Timer({ setTimeOut, questionNumber }) {
-  const [timer, setTimer] = useState(30);
+export default function Counter({ setTimeOut, questionNumber , timer , setTimer}) {
+    
 
-  useEffect(() => {
-    if (timer === 0) return setTimeOut(true);
-    const interval = setInterval(() => {
-      setTimer((prev) => prev - 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [timer, setTimeOut]);
+    useEffect(() => {
+        if (timer === 0) return setTimeOut(true);
+        const interval = setInterval(() => {
+            setTimer((prev) => prev - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [timer]);
 
-  useEffect(() => {
-    setTimer(30);
-  }, [questionNumber]);
-  return timer;
+    useEffect(() => {
+      console.log("kiki");
+        setTimer(30);
+    }, []);
+
+    //useEffect(() => {
+    //   setTimer(30);
+    //}, [questionNumber]);
+
+    return (<div>{timer}</div>);
 }
