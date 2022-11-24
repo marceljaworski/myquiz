3485f3cbc413ad1fef3333cc3c48ffe1a7a3083e
Marcelearn <jaworskimarcel@gmail.com>
2022-11-24 09:43:25 +0100

    Info view

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <div className='Header' >
       <Link className='startLink' to="/">Start</Link>
+      <Link className='startLink' to="/info">Info</Link>
     </div>
   )
 }
